bd0de0facab70a9319f20e803953d956e1c6fc72
IndraJeet09 <indrajeetchouhan680@gmail.com>
2026-08-14 12:40:18 +0530

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Button } from './components/Button'
 import  Card  from './components/Card'
+import { CreateContent } from './components/CreateContent'
 import { PlusIcon } from './icons/PlusIcon'
 import { ShareIcon } from './icons/ShareIcon'
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className='p-4'>
+      <CreateContent open={true}/>
       <div className='flex justify-end gap-4'>
       <Button variant='primary' text="Add Content" startIcon={<PlusIcon/>}></Button>
       <Button variant='secondary' text='Share Brain' startIcon={<ShareIcon/>}></Button>
---
+++ src/components/CreateContent.tsx
@@ -0,0 +1,15 @@
+
+export function CreateContent({open, onClose}){
+
+    return <div>
+    
+        {open && <div className="w-screen h-screen bg-slate-600 fixed top-0 left-0 opacity-80 flex justify-center">
+            <div className="flex flex-col justify-center">
+                <span className="bg-white opacity-100 p-4 rounded">
+                    
+                </span>
+            </div>
+        </div>}
+    </div>
+
+}
