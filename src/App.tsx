b6d963ee9247c5c7652fbc10d0798a78394080cb
IndraJeet09 <indrajeetchouhan680@gmail.com>
2026-08-14 12:08:38 +0530

    completed card component

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ import { ShareIcon } from './icons/ShareIcon'
 function App() {
 
   return (
-    <div >
+    <div className='p-4'>
+      <div className='flex justify-end gap-4'>
       <Button variant='primary' text="Add Content" startIcon={<PlusIcon/>}></Button>
       <Button variant='secondary' text='Share Brain' startIcon={<ShareIcon/>}></Button>
-
+      </div>
+      <div className='flex gap-4'>
       <Card type='twitter' link='https://x.com/xKhalifan/status/2076011903357452465' title='BRAINMAXXING'/>
       <Card type='youtube' link='https://www.youtube.com/embed/4MAupwjl3pc?si=5ERrVqyJM7CdQpff' title='Art of Wining in TECH'/>
+
+      </div>
     </div>
   )
 }
