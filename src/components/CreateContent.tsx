cfb1faa9cbc64425f8f4e43dfc2f3dd948336197
IndraJeet09 <indrajeetchouhan680@gmail.com>
2026-08-14 13:11:43 +0530

    creating new component

--- src/components/CreateContent.tsx
+++ src/components/CreateContent.tsx
@@ -6,7 +6,7 @@ export function CreateContent({open, onClose}){
         {open && <div className="w-screen h-screen bg-slate-600 fixed top-0 left-0 opacity-80 flex justify-center">
             <div className="flex flex-col justify-center">
                 <span className="bg-white opacity-100 p-4 rounded">
-                    
+                    hi there
                 </span>
             </div>
         </div>}
