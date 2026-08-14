d7f94f49450f0bb14dcf9672830e06240b944be4
IndraJeet09 <indrajeetchouhan680@gmail.com>
2026-08-14 12:00:21 +0530

    added card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Button } from './components/Button'
-import { Card } from './components/Card'
+import  Card  from './components/Card'
 import { PlusIcon } from './icons/PlusIcon'
 import { ShareIcon } from './icons/ShareIcon'
 
@@ -8,10 +8,11 @@ function App() {
 
   return (
     <div >
-      <Button variant='primary' text="Add-Content" startIcon={<PlusIcon/>}></Button>
-      <Button variant='secondary' text='Share-Brain' startIcon={<ShareIcon/>}></Button>
+      <Button variant='primary' text="Add Content" startIcon={<PlusIcon/>}></Button>
+      <Button variant='secondary' text='Share Brain' startIcon={<ShareIcon/>}></Button>
 
-      <Card />
+      <Card type='twitter' link='https://x.com/xKhalifan/status/2076011903357452465' title='BRAINMAXXING'/>
+      <Card type='youtube' link='https://www.youtube.com/embed/4MAupwjl3pc?si=5ERrVqyJM7CdQpff' title='Art of Wining in TECH'/>
     </div>
   )
 }
